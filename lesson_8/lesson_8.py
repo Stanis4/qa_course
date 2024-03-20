87b4f51905428b47dde9808dabd68f97930b3998
Stanislav <kutyaev.stass@gmail.com>
2024-03-20 22:53:22 +0200

    Completed Homework 8 (refactored conversion function)

--- lesson_8/lesson_8.py
+++ lesson_8/lesson_8.py
@@ -9,30 +9,24 @@ UAH_USD_RATE = 38.5
 
 def read_csv():
     file_path = os.path.abspath("test_file.csv")
-    data = []
 
     with open(file=file_path, newline='', mode='r') as file:
         rows = csv.reader(file)
-        for row in rows:
-            data.append(row)
+        data = list(rows)
     return data
-
-
-def _usd_to_uah(data):
-    refined_data = [[int(item) if item.isdigit() else item for item in row] for row in data]
-    converted_list = [[str(item * UAH_USD_RATE) if isinstance(item, int) else item for item in row] for row in
-                      refined_data]
-    return converted_list
 
 
 def convert_to_uah():
     data = read_csv()
-    data_with_converted_values = _usd_to_uah(data)
     new_file = os.getcwd() + "/salaries_uah.csv"
+
+    for row in data[1:]:
+        for item in range(1, len(row)):
+            row[item] = round(int(row[item]) * UAH_USD_RATE)
 
     with open(file=new_file, newline='', mode='w') as file:
         rows = csv.writer(file)
-        rows.writerows(data_with_converted_values)
+        rows.writerows(data)
 
 
 convert_to_uah()
