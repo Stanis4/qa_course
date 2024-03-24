138caf48f66f796d37fd0684f8d995d879a99904
Stanislav <kutyaev.stass@gmail.com>
2024-03-24 15:33:01 +0200

    Completed Homework 9

---
+++ lesson_9/lesson_9.py
@@ -0,0 +1,30 @@
+"""1. Напишіть генератор котрий буде працювати як range() із агрументами start, stop & step"""
+
+
+def custom_range_generator(start: int = 0, stop: int = 1, step: int = 1):
+    if step == 0:
+        raise ValueError("Zero step is not possible")
+
+    if start < stop and step > 0:
+        value = start
+        while value < stop:
+            yield value
+            value += step
+    elif start > stop and step < 0:
+        value = start
+        while value > stop:
+            yield value
+            value += step
+    else:
+        raise ValueError("Unacceptable values are entered")
+
+
+for item in custom_range_generator(10, 1, -1):
+    print(item)
+
+
+"""2. Напишіть generator comprehansion котрий буде повертати числа від 1 до 10"""
+
+gen_comprehension = (item for item in custom_range_generator(1, 11))
+for item in gen_comprehension:
+    print(item)
