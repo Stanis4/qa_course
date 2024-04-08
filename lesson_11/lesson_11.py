b0c7da10cec01f57d153fd2c70df7c1a767d62b7
Stanislav <kutyaev.stass@gmail.com>
2024-04-08 13:11:34 +0300

    Completed Homework 11 - refactored v2

--- lesson_11/lesson_11.py
+++ lesson_11/lesson_11.py
@@ -2,6 +2,7 @@
 
 
 class Laptop:
+    shop = 'Amazon'
 
     def __init__(self, brand: str, gpu: int):
         self.__brand = brand
@@ -15,32 +16,15 @@ class Laptop:
     def brand(self, new_brand):
         self.__brand = new_brand
 
-    @property
-    def available_brands(self):
-        """
-        Method returns dict of available brands and number of items for each brand
-        """
-        brands = {'apple': 10, 'asus': 11, 'lenovo': 15, 'hp': 20, 'dell': 1, 'msi': 8}
-        return brands
-
     def show_default_browser(self):
         return "Default browser is Edge"
-
-    def check_items_left(self, brand):
-        if self.brand in self.available_brands:
-            return f"Number of {brand.capitalize()} laptops: {self.available_brands[brand]}"
-
-    @classmethod
-    def set_brand_items(cls, brand: str, number: int):
-        if brand in cls.available_brands:
-            cls.available_brands[brand] = number
 
 
 """2. Створити клас котрий буде успадковувати клас з першого завдання, та додати новий метод у CHILD клас"""
 
 
 class Apple(Laptop):
-    def __init__(self, brand, gpu, browser):
+    def __init__(self, brand: str, gpu: int, browser: str):
         self.browser = browser
         super().__init__(brand, gpu)
 
