2e061971d161865dc3ea6a5991d935e4ef390451
Stanislav <kutyaev.stass@gmail.com>
2024-04-08 12:48:30 +0300

    Completed Homework 11 - refactored

--- lesson_11/lesson_11.py
+++ lesson_11/lesson_11.py
@@ -2,49 +2,47 @@
 
 
 class Laptop:
-    # brands and number of items
-    brands = {'apple': 10, 'asus': 11, 'lenovo': 15, 'hp': 20, 'dell': 1, 'msi': 8}
 
     def __init__(self, brand: str, gpu: int):
-        self.brand = brand
-        self.gpu = gpu
+        self.__brand = brand
+        self.__gpu = gpu
 
-    def get_entered_values(self):
-        return f"Brand: {self.brand}, gpu: {self.gpu}"
+    @property
+    def brand(self):
+        return self.__brand
+
+    @brand.setter
+    def brand(self, new_brand):
+        self.__brand = new_brand
+
+    @property
+    def available_brands(self):
+        """
+        Method returns dict of available brands and number of items for each brand
+        """
+        brands = {'apple': 10, 'asus': 11, 'lenovo': 15, 'hp': 20, 'dell': 1, 'msi': 8}
+        return brands
 
     def show_default_browser(self):
         return "Default browser is Edge"
 
     def check_items_left(self, brand):
-        if self.brand in self.brands:
-            return f"Number of {brand.capitalize()} laptops: {self.brands[brand]}"
-
-    def set_gpu(self, new_gpu):
-        self.gpu = new_gpu
+        if self.brand in self.available_brands:
+            return f"Number of {brand.capitalize()} laptops: {self.available_brands[brand]}"
 
     @classmethod
     def set_brand_items(cls, brand: str, number: int):
-        if brand in cls.brands:
-            cls.brands[brand] = number
+        if brand in cls.available_brands:
+            cls.available_brands[brand] = number
 
-
-laptop = Laptop('hp', 16)
-print(laptop.get_entered_values())
-
-laptop.set_gpu(24)
-print(laptop.get_entered_values())
-
-print(laptop.check_items_left('apple'))
-laptop.set_brand_items('apple', 9)
-print(laptop.check_items_left('apple'))
 
 """2. Створити клас котрий буде успадковувати клас з першого завдання, та додати новий метод у CHILD клас"""
 
 
 class Apple(Laptop):
     def __init__(self, brand, gpu, browser):
-        super().__init__(brand, gpu)
         self.browser = browser
+        super().__init__(brand, gpu)
 
     def preorder(self):
         return "Pre-order successfully accepted"
